a9cc3de20c56d453d51e1a6005a1117f5c2cc01d
prachi-s04 <prachisingh1604@gmail.com>
2024-09-22 23:56:00 +0530

    deployment process

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride= require("method-override");
 const ejsMate= require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const session=require("express-session");
+const MongoStore = require("connect-mongo");
 const flash =require("connect-flash");
 const passport=require("passport");
 const LocalStrategy = require("passport-local")
@@ -19,7 +20,8 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
-const MONGO_URL = "mongodb://localhost:27017/wanderquest";
+//const MONGO_URL = "mongodb://localhost:27017/wanderquest";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
     .then(() => {
@@ -30,7 +32,7 @@ main()
     });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine","ejs");
@@ -40,7 +42,20 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret:"mysupersecretcode"
+    },
+    touchAfter: 24*3600,
+});
+
+store.on("error", () => {
+    console.log("ERROR IN MONGO STORE", err);
+})
+
 const sessionOptions ={
+    store,
     secret: "mysupersecretcode",
     resave: false,
     saveUninitialized: true,
@@ -50,9 +65,6 @@ const sessionOptions ={
         httpOnly: true
     },
 };
-app.get("/", (req, res) => {
-    res.send("hi, i am root");
-});
 
 app.use(session(sessionOptions));
 app.use(flash());
